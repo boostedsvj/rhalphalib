3f47bdf6db79b6fd8388306856ddfdfe3ec6d2d0
Nick Smith <nick.smith@cern.ch>
2020-07-16 21:33:00 -0500

    More fromiter methods

--- rhalphalib/util.py
+++ rhalphalib/util.py
@@ -138,13 +138,14 @@ def install_roofit_helpers():
 
     _ROOT.RooAbsCollection.assign = _RooAbsCollection_assign
 
-    def _RooArgList_fromiter(cls, iterable, silent=False):
+    def _cls_fromiter(cls, iterable, silent=False):
         items = cls()
         for item in iterable:
             items.add(item, silent)
         return items
 
-    _ROOT.RooArgList.fromiter = classmethod(_RooArgList_fromiter)
+    _ROOT.RooArgList.fromiter = classmethod(_cls_fromiter)
+    _ROOT.RooArgSet.fromiter = classmethod(_cls_fromiter)
 
     def _RooAbsReal__add__(self, other):
         '''
